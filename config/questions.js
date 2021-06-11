0011a5e7467b472d2d825f0f0118408c6a58e504
Paargav Shanker <paargav.shuttle@gmail.com>
2021-06-11 19:25:54 +0530

    added check for game over

--- config/questions.js
+++ config/questions.js
@@ -1,61 +1,61 @@
 const questions = [
         { 
                 question: "This is the mock for question1",
-                clue1: null,
+                clue1: "Answer is answer1",
                 clue2: null,
                 answer: "answer1"
         },
         {
                 question: "This is the mock for question2",
-                clue1: null,
+                clue1: "Answer is answer2",
                 clue2: null,
                 answer: "answer2"
         },
         {
                 question: "This is the mock for question3",
-                clue1: null,
+                clue1: "Answer is answer3",
                 clue2: null,
                 answer: "answer3"
         },
         {
                 question: "This is the mock for question4",
-                clue1: null,
+                clue1: "Answer is answer4",
                 clue2: null,
                 answer: "answer4"
         },
         {
                 question: "This is the mock for question5",
-                clue1: null,
+                clue1: "Answer is answer5",
                 clue2: null,
                 answer: "answer5"
         },
         {
                 question: "This is the mock for question6",
-                clue1: null,
+                clue1: "Answer is answer6",
                 clue2: null,
                 answer: "answer6"
         },
         {
                 question: "This is the mock for question7",
-                clue1: null,
+                clue1: "Answer is answer7",
                 clue2: null,
                 answer: "answer7"
         },
         {
                 question: "This is the mock for question8",
-                clue1: null,
+                clue1: "Answer is answer8",
                 clue2: null,
                 answer: "answer8"
         },
         {
                 question: "This is the mock for question9",
-                clue1: null,
+                clue1: "Answer is answer9",
                 clue2: null,
                 answer: "answer9"
         },
         {
                 question: "This is the mock for question10",
-                clue1: null,
+                clue1: "Answer is answer10",
                 clue2: null,
                 answer: "answer10"
         }
